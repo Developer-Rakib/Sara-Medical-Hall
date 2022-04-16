6bda64a0e5adb50d3409d8a435a4b9a04c447ada
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-17 04:05:00 +0600

    require auth component added

--- src/pages/Home/Banner/Bannner.js
+++ src/pages/Home/Banner/Bannner.js
@@ -6,7 +6,7 @@ const Bannner = () => {
         <div className='banner-container text-left px-20 flex justify-center flex-col'>
             <h5 className="text-lg">Psychology specialist</h5>
             <h1 className="text-7xl">Dr. Sara Dowson</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa sunt voluptatem sit ea mollitia tempora? Dolores excepturi praesentium voluptatum veniam.</p>
+            <p>Mollis aliquam ut porttitor leo a diam. Ac tortor vitae purus faucibus ornare. Sapien faucibus et molestie ac feugiat. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. .</p>
             <button>Read More</button>
         </div>
     );
